c3181d1751e77b118cd6f5c535c2abd5e69d8cfe
ander1119 <b07902060@ntu.edu.tw>
2020-04-21 14:04:20 +0800

    bug fix

--- scheduler.c
+++ scheduler.c
@@ -36,7 +36,7 @@ void schedule(struct Process processList[], int processNum, int schedulingPolicy
 		}
 
 		if(runningIndex != -1 && processList[runningIndex].execTime == 0){ //the running process ends
-			wait(processList[runningIndex].pid);
+			wait(processList[runningIndex].pid, NULL);
 			printf("%s %d\n", processList[runningIndex].processName, processList[runningIndex].pid);
 			processList[runningIndex].status = DONED;
 			runningIndex = -1;
