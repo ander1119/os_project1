0883cb73808e5cdf1a54b45d404d714651740372
ander1119 <b07902060@ntu.edu.tw>
2020-04-23 23:38:52 +0800

    scheduler

--- process.c
+++ process.c
@@ -38,9 +38,9 @@ void setHighPriority(int pid){
 
 void setLowPriority(int pid){
 	struct sched_param param;
-	param.sched_priority = 0;
+	param.sched_priority = 99;
 	sched_yield();
-	if(sched_setscheduler(pid, SCHED_OTHER, &param) < 0){
+	if(sched_setscheduler(pid, SCHED_FIFO, &param) < 0){
 		fprintf(stderr, "can't set pid = %d low priority\n", pid);
 		exit(0);
 	}
