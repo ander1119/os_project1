8855aea3648a1b9953e686b5e7cef11667ce3bac
ander1119 <b07902060@ntu.edu.tw>
2020-04-23 15:38:05 +0800

    SJF

--- process.c
+++ process.c
@@ -54,13 +54,13 @@ int initProcess(int execTime){
 		syscall(GETTIME, &startSec, &startNSec);
 		
 		for(int i=0 ; i<execTime ; i++){
-/*
+
 #ifdef DEBUG
 			if(i % 100 == 0){
 				fprintf(stderr, "pid = %d run %d unit time\n", getpid(), i);
 			}
 #endif
-*/
+
 			unitTime();
 		}
 		syscall(GETTIME, &finishSec, &finishNSec);
