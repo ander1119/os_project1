1ac144e062bd188760edd391c639a968ab35ac8c
ander1119 <b07902060@ntu.edu.tw>
2020-04-25 17:08:17 +0800

    .sh

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ do
 	outputFilePath="output/${inputFileName}_stdout.txt"
 	dmesgFilePath="output/${inputFileName}_dmesg.txt"
 
-	sudo ./main < "${inputFileName}" 2>/dev/null > "${outputFilePath}"
+	sudo ./main < "${FILE}" 2>/dev/null > "${outputFilePath}"
 	sudo dmesg -c | grep Project1 > "${dmesgFilePath}"
 done
 
